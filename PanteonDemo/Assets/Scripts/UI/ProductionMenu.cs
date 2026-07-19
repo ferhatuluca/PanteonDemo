6f0a1ab5b90a5ab0e079753cc3a475338f1f3a25
Reber Ferhat Uluca <ferhat.1353@hotmail.com>
2026-07-19 21:00:42 +0300

    Update ProductionMenu.cs

--- PanteonDemo/Assets/Scripts/UI/ProductionMenu.cs
+++ PanteonDemo/Assets/Scripts/UI/ProductionMenu.cs
@@ -14,7 +14,12 @@ namespace UI
 		[SerializeField] private RectTransform _contentPanelTransform;
 		[SerializeField] private VerticalLayoutGroup _verticalLayoutGroup;
 
-		private RectTransform[] _buildingUIRects;
+		private RectTransform _firstItem;
+		private float _totalRepeatedItemHeight;
+		private float _totalItemAllHeight;
+
+		private Vector2 _oldVelocity;
+		private bool _isUpdated;
 		
 		private void Start()
 		{
@@ -26,17 +31,48 @@ namespace UI
 			TeamType[] teamTypes = (TeamType[])Enum.GetValues(typeof(TeamType));
 			BuildingData[] buildingData = GameManager.Instance.GeneralData.BuildingData;
 			
-			_buildingUIRects = new RectTransform[buildingData.Length * teamTypes.Length];
-
-			int count = 0;
 			foreach (TeamType teamType in teamTypes)
 			{
 				foreach (BuildingData data in buildingData)
 				{
 					BuildingUI ui = Instantiate(data.BuildingUIPrefab, _contentPanelTransform);
 					ui.Init(data, teamType);
-					_buildingUIRects[count++] = ui.GetComponent<RectTransform>();
+					if(_firstItem == null)
+						_firstItem = ui.GetComponent<RectTransform>();
 				}
+			}
+			
+			int repeatedItemCount = buildingData.Length;
+			int totalItemCount = buildingData.Length * teamTypes.Length;
+			float oneItemHeight = _firstItem.rect.height + _verticalLayoutGroup.spacing;
+
+			_totalRepeatedItemHeight = repeatedItemCount * oneItemHeight;
+			_totalItemAllHeight = totalItemCount * oneItemHeight;
+			
+			_contentPanelTransform.localPosition = new Vector3(0f, _totalRepeatedItemHeight, 0f);
+		}
+
+		private void Update()
+		{
+			if (_isUpdated)
+			{
+				_isUpdated = false;
+				_scrollRect.velocity = _oldVelocity;
+			}
+			
+			if (_contentPanelTransform.localPosition.y > _totalItemAllHeight)
+			{
+				Canvas.ForceUpdateCanvases();
+				_oldVelocity = _scrollRect.velocity;
+				_contentPanelTransform.localPosition -= new Vector3(0f, _totalRepeatedItemHeight, 0f);
+				_isUpdated = true;
+			}
+			if (_contentPanelTransform.localPosition.y < 0)
+			{
+				Canvas.ForceUpdateCanvases();
+				_oldVelocity = _scrollRect.velocity;
+				_contentPanelTransform.localPosition += new Vector3(0f, _totalRepeatedItemHeight * _totalRepeatedItemHeight, 0f);
+				_isUpdated = true;
 			}
 		}
 	}
