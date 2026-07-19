231739045dc4dc2a842c0b89581b104b9ae2d65f
Reber Ferhat Uluca <ferhat.1353@hotmail.com>
2026-07-19 21:43:53 +0300

    bug fixes

--- PanteonDemo/Assets/Scripts/UI/ProductionMenu.cs
+++ PanteonDemo/Assets/Scripts/UI/ProductionMenu.cs
@@ -7,73 +7,78 @@ using UnityEngine.UI;
 
 namespace UI
 {
-	public class ProductionMenu : MonoBehaviour
-	{
-		[SerializeField] private ScrollRect _scrollRect;
-		[SerializeField] private RectTransform _viewPortTransform;
-		[SerializeField] private RectTransform _contentPanelTransform;
-		[SerializeField] private VerticalLayoutGroup _verticalLayoutGroup;
+    public class ProductionMenu : MonoBehaviour
+    {
+        [SerializeField] private ScrollRect _scrollRect;
+        [SerializeField] private RectTransform _contentPanelTransform;
+        [SerializeField] private VerticalLayoutGroup _verticalLayoutGroup;
 
-		private RectTransform _firstItem;
-		private float _totalRepeatedItemHeight;
-		private float _totalItemAllHeight;
+        private RectTransform _firstItem;
+        private float _totalRepeatedItemHeight;
 
-		private Vector2 _oldVelocity;
-		private bool _isUpdated;
-		
-		private void Start()
-		{
-			SpawnBuildingUIs();
-		}
+        private Vector2 _oldVelocity; 
+        private bool _isUpdated;
 
-		private void SpawnBuildingUIs()
-		{
-			TeamType[] teamTypes = (TeamType[])Enum.GetValues(typeof(TeamType));
-			BuildingData[] buildingData = GameManager.Instance.GeneralData.BuildingData;
-			
-			foreach (TeamType teamType in teamTypes)
-			{
-				foreach (BuildingData data in buildingData)
-				{
-					BuildingUI ui = Instantiate(data.BuildingUIPrefab, _contentPanelTransform);
-					ui.Init(data, teamType);
-					if(_firstItem == null)
-						_firstItem = ui.GetComponent<RectTransform>();
-				}
-			}
-			
-			int repeatedItemCount = buildingData.Length;
-			int totalItemCount = buildingData.Length * teamTypes.Length;
-			float oneItemHeight = _firstItem.rect.height + _verticalLayoutGroup.spacing;
+        private void Start()
+        {
+            SpawnBuildingUIs();
+        }
+        
+        private void Update()
+        {
+            if (_isUpdated)
+            {
+                _isUpdated = false; 
+                _scrollRect.velocity = _oldVelocity;
+            }
+            
+            Vector2 position = _contentPanelTransform.anchoredPosition;
+            if (position.y < _totalRepeatedItemHeight * 0.5f)
+            {
+                _oldVelocity = _scrollRect.velocity;
+                position.y += _totalRepeatedItemHeight;
+                _contentPanelTransform.anchoredPosition = position;
+                _isUpdated = true;
+            }
+            else if (position.y > _totalRepeatedItemHeight * 1.5f)
+            {
+                _oldVelocity = _scrollRect.velocity;
+                position.y -= _totalRepeatedItemHeight;
+                _contentPanelTransform.anchoredPosition = position;
+                _isUpdated = true;
+            }
+        }
 
-			_totalRepeatedItemHeight = repeatedItemCount * oneItemHeight;
-			_totalItemAllHeight = totalItemCount * oneItemHeight;
-			
-			_contentPanelTransform.localPosition = new Vector3(0f, _totalRepeatedItemHeight, 0f);
-		}
+        private void SpawnBuildingUIs()
+        {
+            TeamType[] teamTypes = (TeamType[])Enum.GetValues(typeof(TeamType));
+            BuildingData[] buildingData = GameManager.Instance.GeneralData.BuildingData;
+            
+            for (int repeat = 0; repeat < 3; repeat++)
+            {
+                foreach (TeamType teamType in teamTypes)
+                {
+                    foreach (BuildingData data in buildingData)
+                    {
+                        BuildingUI ui = Instantiate(data.BuildingUIPrefab, _contentPanelTransform);
+                        ui.Init(data, teamType);
 
-		private void Update()
-		{
-			if (_isUpdated)
-			{
-				_isUpdated = false;
-				_scrollRect.velocity = _oldVelocity;
-			}
-			
-			if (_contentPanelTransform.localPosition.y > _totalItemAllHeight)
-			{
-				Canvas.ForceUpdateCanvases();
-				_oldVelocity = _scrollRect.velocity;
-				_contentPanelTransform.localPosition -= new Vector3(0f, _totalRepeatedItemHeight, 0f);
-				_isUpdated = true;
-			}
-			if (_contentPanelTransform.localPosition.y < 0)
-			{
-				Canvas.ForceUpdateCanvases();
-				_oldVelocity = _scrollRect.velocity;
-				_contentPanelTransform.localPosition += new Vector3(0f, _totalRepeatedItemHeight, 0f);
-				_isUpdated = true;
-			}
-		}
-	}
+                        if (_firstItem == null)
+                            _firstItem = ui.GetComponent<RectTransform>();
+                    }
+                }
+            }
+
+            int repeatedItemCount = buildingData.Length * teamTypes.Length;
+            float oneItemHeight = _firstItem.rect.height + _verticalLayoutGroup.spacing;
+
+            _totalRepeatedItemHeight = repeatedItemCount * oneItemHeight;
+            
+            Vector2 pos = _contentPanelTransform.anchoredPosition;
+            pos.y = _totalRepeatedItemHeight;
+            _contentPanelTransform.anchoredPosition = pos;
+            
+            Canvas.ForceUpdateCanvases();
+        }
+    }
 }
