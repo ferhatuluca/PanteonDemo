237d6791c8b172a8832815c7b0fc1820ab1ed9c2
Reber Ferhat Uluca <ferhat.1353@hotmail.com>
2026-07-19 22:09:39 +0300

    update

--- PanteonDemo/Assets/Scripts/UI/InformationPanel.cs
+++ PanteonDemo/Assets/Scripts/UI/InformationPanel.cs
@@ -1,8 +1,17 @@
-﻿using UnityEngine;
+﻿using System;
+using Core.Enums;
+using Core.Utilities.Pool_Spawner.Spawner.SpawnerWithPool;
+using UnityEngine;
 
 namespace UI
 {
-	public class InformationPanel : MonoBehaviour
+	public class InformationPanel : SpawnerMonoWithPoolWithType<SoldierUI, SoldierType>
 	{
+		[SerializeField] private RectTransform _contentPanelTransform;
+		
+		private void Start()
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
--- PanteonDemo/Assets/Scripts/UI/SoldierUI.cs
+++ PanteonDemo/Assets/Scripts/UI/SoldierUI.cs
@@ -1,12 +1,44 @@
 ﻿using System;
 using Core.Enums;
 using Core.Scriptables;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace UI
 {
-	public class SoldierUI : MonoBehaviour
+	public class SoldierUI : MonoBehaviour, ISold
 	{
 		public static event Action<SoldierData, TeamType> OnSoldierUIClicked;
+
+		[SerializeField] private Image _icon;
+		[SerializeField] private TextMeshProUGUI _name;
+
+		private SoldierData _soldierData;
+		private TeamType _teamType;
+		
+		private Button _button;
+
+		private void Awake()
+		{
+			_button = GetComponent<Button>();
+			_button.onClick.AddListener(OnButtonClicked);
+		}
+
+		public void Init(SoldierData soldierData, TeamType teamType)
+		{
+			_soldierData = soldierData;
+			_teamType = teamType;
+			
+			_name.text = soldierData.Name;
+			
+			SoldierTeamData teamData = soldierData.GetSoldierTeamData(teamType);
+			_icon.sprite = teamData.Icon;
+		}
+		
+		private void OnButtonClicked()
+		{
+			OnSoldierUIClicked?.Invoke(_soldierData, _teamType);
+		}
 	}
 }
